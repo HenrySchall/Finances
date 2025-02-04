2f384a5554bdebff18655e0233435db613a07a87
Henrique Werner <henrique.schall@outlook.com>
2025-02-04 18:01:28 -0300

    .

--- 2.Risco/1.Curva de Juros.py
+++ 2.Risco/1.Curva de Juros.py
@@ -1,6 +1,5 @@
 # pip install xlrd
 # pip install bizdays
-# pip install polars
 
 import pandas as pd
 import requests
@@ -23,9 +22,9 @@ def brazil_calendar():
     calendar = Calendar(holidays, weekends)
     return calendar
 
-#########################################
-## Obtaining DI Futures Contracts data ##
-#########################################
+####################################
+## Obtaining DI Futures Contracts ##
+####################################
 
 cal = brazil_calendar()
 
@@ -43,19 +42,14 @@ df = pd.DataFrame([[i.text for i in l] for l in lines])
 
 print(df)
 
-df = df[[0, 1, 3]]
-df.columns = ['Ativo', 'Cod_Vencimento', 'ValorAjuste']
+df = df[[0, 1, 3]] #Select Columns
+df.columns = ['Ativo', 'Cod_Vencimento', 'ValorAjuste'] #Rename Columns
 
-df['ValorAjuste'] = df['ValorAjuste'].str.replace('.', '').str.replace(',', '.').astype(float)
+df['ValorAjuste'] = df['ValorAjuste'].str.replace('.', '').str.replace(',', '.').astype(float) #Replace , for .
 
-df2 = df.iloc[223:263]
+df = df.iloc[223:263] #Select DI value lines
 
-print(df2)
-
-df2['Ativo'] = np.nan
-df2['Ativo'] = df2['Ativo'].fillna('DI1_DI_de_1_dia')
-
-print(df2)
-
-df2 = df2[df2['Ativo'].str.startswith('DI1')]
-df = df2
+df['Ativo'] = np.nan
+df['Ativo'] = df['Ativo'].fillna('DI1')
+df['CodVcto'] = df['CodVcto'].str.strip() # Remove the empty spaces from the expiration code column
+df = df[df['Ativo'].str.startswith('DI1')] #
